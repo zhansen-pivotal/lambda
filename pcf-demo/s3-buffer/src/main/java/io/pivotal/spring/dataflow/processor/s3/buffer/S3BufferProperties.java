256f3641860095aa0d6e22cad935daec667483eb
Zac Hansen <zhansen@pivotal.io>
2016-08-24 17:19:26 -0600

    added @ConfigurationProperties

--- pcf-demo/s3-buffer/src/main/java/io/pivotal/spring/dataflow/processor/s3/buffer/S3BufferProperties.java
+++ pcf-demo/s3-buffer/src/main/java/io/pivotal/spring/dataflow/processor/s3/buffer/S3BufferProperties.java
@@ -1,7 +1,9 @@
 package io.pivotal.spring.dataflow.processor.s3.buffer;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@ConfigurationProperties
 public class S3BufferProperties {
 	
 	@Value("${aggSize:100}")
