4853404d53428996b947b2d1c7d8f1de2fe65d9a
Zac Hansen <zhansen@pivotal.io>
2016-12-08 13:08:26 -0700

    added whitelist properties

--- pcf-demo/curration-processor/src/main/java/io/pivotal/spring/dataflow/processor/curration/CurrationProcessorProperties.java
+++ pcf-demo/curration-processor/src/main/java/io/pivotal/spring/dataflow/processor/curration/CurrationProcessorProperties.java
@@ -3,7 +3,7 @@ package io.pivotal.spring.dataflow.processor.curration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties
+@ConfigurationProperties("demoproperties")
 public class CurrationProcessorProperties {
 
 	@Value("${dataSource:unknown}")
